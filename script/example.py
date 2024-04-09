7e294d9f7860bca9d23f4cfe1379e02eb64caf28
chao <xc17671600218@gmail.com>
2024-04-09 20:56:38 +0800

    upload sell stratge

--- script/example.py
+++ script/example.py
@@ -4,7 +4,10 @@ from __future__ import (absolute_import, division, print_function,
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys,time  # To find out the script name (in argv[0])
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 # Import the backtrader platform
 import backtrader as bt
@@ -12,11 +15,18 @@ import backtrader as bt
 from load_binance_csv import BINACSVData
 import btc_enum
 
+
 class TradeResult():
     def __init__(self) -> None:
         self.times = 0
 
+class MvDirection():
+    Unknown = 0
+    Up = 1
+    Down = 2
+
 _cash = 10000
+_key_point_width=300
 
 # Create a Stratey
 class TestStrategy(bt.Strategy):
@@ -55,7 +65,7 @@ class TestStrategy(bt.Strategy):
         self.short_wrong_times = 0
         self.long_make_times = 0
         self.long_wrong_times = 0
-
+        # self.data_analyze()
         # Add a MovingAverageSimple indicator
         self.sma_short = bt.indicators.SimpleMovingAverage(
             self.datas[0], period=7)
@@ -65,7 +75,17 @@ class TestStrategy(bt.Strategy):
         
         self.sma_long = bt.indicators.SimpleMovingAverage(
             self.datas[0], period=99)
-        
+
+        self.smasig = bt.ind.CrossOver(self.sma_short, self.sma_middle)
+
+        macd = bt.ind.MACD(self.datas[0],
+                           period_me1=25,
+                           period_me2=99,
+                           period_signal=2)
+
+        # Cross of macd.macd and macd.signal
+        self.macdsig = bt.ind.CrossOver(macd.macd, macd.signal)
+
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
             # Buy/Sell order submitted/accepted to/by broker - Nothing to do
@@ -85,7 +105,7 @@ class TestStrategy(bt.Strategy):
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
                 if self.user_position == btc_enum.Position.Short:
-                    if self.sellprice < self.buyprice:
+                    if self.sellprice > self.buyprice:
                         self.short_make_times = self.short_make_times + 1
                     else: self.short_wrong_times = self.short_wrong_times+1
             else:  # Sell
@@ -96,7 +116,7 @@ class TestStrategy(bt.Strategy):
                 self.order_low_price = self.dataclose[0]
                 self.sellprice = order.executed.price
                 if self.user_position == btc_enum.Position.Long:
-                    if self.sellprice > self.buyprice:
+                    if self.sellprice < self.buyprice:
                         self.long_make_times = self.long_make_times + 1
                     else: self.long_wrong_times = self.long_wrong_times+1
 
@@ -124,15 +144,13 @@ class TestStrategy(bt.Strategy):
         self.data_offset = self.data_offset + 1
         if self.order:
             return
-
         # Check if we are in the market
         if not self.position:
             if self.params.user_scope in [btc_enum.Scope.BUY,btc_enum.Scope.ALL]:
                 # Not yet ... we MIGHT BUY if ...
                 # print(self.dataclose[0],self.sma_short[0])
-                if self.sma_short[0] > self.sma_short[-1] and self.sma_short[-1] > self.sma_short[-2] and  self.sma_short[-2] > self.sma_short[-3] \
-                    and self.sma_middle[0] > self.sma_middle[-1] \
-                    and self.dataclose[0] > self.sma_short[0]:
+                if self.smasig[0] > 0.0 and \
+                    self.sma_middle[0] < self.sma_middle[-1]:
 
                     # BUY, BUY, BUY!!! (with all possible default parameters)
                     self.log('BUY CREATE, %.2f' % self.dataclose[0])
@@ -143,29 +161,31 @@ class TestStrategy(bt.Strategy):
                     self.result.times = self.result.times + 1
                     self.user_position = btc_enum.Position.Long
             elif self.params.user_scope in [btc_enum.Scope.SELL,btc_enum.Scope.ALL]:
-                if self.sma_short[0] < self.sma_short[-1] \
-                    and self.sma_middle[0] < self.sma_middle[-1] \
-                    and (self.dataopen[0]-self.dataclose[0])> self.average_range()*2 \
-                    and self.volume[0] > self.average_volume()*2 :
+                if self.smasig[0] < 0.0 :
+                    s_perid_a,b=self.linear_fitting(self.dataclose,7)
+                    m_perid_a,b=self.linear_fitting(self.dataclose,25)
+                    high_a,high_b=self.linear_fitting(self.datahigh,7)
+                    low_a,low_b=self.linear_fitting(self.datalow,7)
+                    average=self.average_range(25)
+                    # print("a:{},average_range:{}".format(s_perid_a,average))
+                    # if m_perid_a - s_perid_a >average*0.5 \
+                    #     and high_a<=0 and low_a<0:
+                    if m_perid_a < 0 and high_a<0 and low_a<0\
+                        and self.average_volume(3) < self.average_volume(99)*3 \
+                        and self.average_volume(3) > self.average_volume(99)*1.3 \
+                        and abs(m_perid_a) > average*0.029:
+                        # and m_perid_a - s_perid_a >average*0.1 \
+                        self.sell(size=self.order_size())
+                        # BUY, BUY, BUY!!! (with all possible default parameters)
+                        self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
-                    # BUY, BUY, BUY!!! (with all possible default parameters)
-                    self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                    # Keep track of the created order to avoid a 2nd order
-                    self.order = self.sell(size=self.order_size())
-                    self.order_low_price = self.dataclose[0]
-                    self.result.times = self.result.times + 1
-                    self.user_position = btc_enum.Position.Short
+                        # Keep track of the created order to avoid a 2nd order
+                        self.order_low_price = self.dataclose[0]
+                        self.result.times = self.result.times + 1
+                        self.user_position = btc_enum.Position.Short
         else:   #in market
             if self.user_position ==btc_enum.Position.Long:#Long(做多)
-                if self.datahigh[0] > self.order_high_price:
-                    self.order_high_price = self.datahigh[0]
-
-                if (self.order_high_price -self.buyprice)/self.buyprice < 0.003: #0.3%
-                    self.log('ignore small change !, %.2f' % self.dataclose[0])
-                elif self.datalow[0] < ((self.order_high_price -self.buyprice)*0.618 + self.buyprice):
-                    # SELL, SELL, SELL!!! (with all possible default parameters)
-                    self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                if self.smasig[0] < 0.0:
 
                     # Keep track of the created order to avoid a 2nd order
                     self.order = self.close()
@@ -184,18 +204,19 @@ class TestStrategy(bt.Strategy):
                 #     #  avoid a 2nd order
                 #     self.order = self.close()
             elif self.user_position ==btc_enum.Position.Short: #Short(做空)
+                # if self.smasig[0] > 0.0:
+                #     self.close()
                 if self.dataclose[0] < self.order_low_price:
-                    self.order_low_price = self.dataclose[0]
+                    self.order_low_price = self.datalow[0]
                 if self.dataclose[0] > self.sellprice:
-                    self.close()
+                    # self.close()
                     # print(self.dataclose[0])
-                    # if (self.dataclose[0] - self.sellprice) > 0.003: #0.3%  we are losing!!!!
-                    #     self.close()
-                elif abs(self.sellprice - self.dataclose[0])/self.sellprice < 0.003: #0.3%
+                    if (self.dataclose[0] - self.sellprice) > 0.01: #0.3%  we are losing!!!!
+                        self.close()
+                elif abs(self.sellprice - self.dataclose[0])/self.sellprice < 0.005: #0.3%
                     self.log('ignore small change !, %.2f' % self.dataclose[0])
                 elif self.dataclose[0] < self.sellprice:#we need more
-                    if(self.dataclose[0] - self.order_low_price)/(self.sellprice-self.order_low_price) > 0.5 \
-                        or self.sma_middle[0] > self.sma_middle[-1]:
+                    if(self.dataclose[0] - self.order_low_price)/(self.sellprice-self.order_low_price) > 0.6 :
                         self.close()
                     # elif (self.sellprice-self.dataclose[0])/self.sellprice > 0.01:
                     #     self.close()
@@ -210,20 +231,38 @@ class TestStrategy(bt.Strategy):
     def quit(self):
         self.env.runstop()
 
-    def average_range(self):
+    def average_range(self,size):
         change = 0
-        for i in range(0,99):
+        for i in range(0,size):
             change = change + (self.datahigh[-i]-self.datalow[-i])
-        return (change/99)
+        return (change/size)
 
-    def average_volume(self):
+    def average_volume(self,size):
         volume = 0
-        for i in range(0,99):
+        for i in range(0,size):
             volume = volume + self.volume[-i]
-        return (volume/25)
+        return (volume/size)
 
     def order_size(self):
         return ((self.broker.getvalue()*0.5+  _cash*0.1)/self.dataclose)
+
+    def slop(self):
+        pass
+
+    def data_analyze(self):
+        pass
+
+    def linear_fitting(self,data,length):
+        X = np.array(range(0,length)).reshape(length, 1)
+        data_array= [data[-i] for i in range(length-1,-1,-1)]
+        Y = np.array(data_array).reshape(length, 1)
+        # 建立线性回归模型
+        regr = linear_model.LinearRegression()
+        # 拟合
+        regr.fit(X, Y)
+        # 不难得到直线的斜率、截距
+        a, b = regr.coef_, regr.intercept_
+        return a,b
 
     # def ma_
 
@@ -232,14 +271,19 @@ class TestStrategy(bt.Strategy):
     #         return False
     #     if sma[0] > sma[-1] and sma[-1] > sma[-2] 
 
+    # def  support_resistance():  
+
+
 if __name__ == '__main__':
     currentDateAndTime = time.localtime()
     print(currentDateAndTime)
     # Create a cerebro entity
-    cerebro = bt.Cerebro()
-
+    cerebro = bt.Cerebro(stdstats=True)
+    cerebro.addobserver(bt.observers.Broker)
+    cerebro.addobserver(bt.observers.Trades)
+    cerebro.addobserver(bt.observers.BuySell)
     # Add a strategy
-    strats = cerebro.optstrategy(
+    strats = cerebro.addstrategy(
         TestStrategy,
         maperiod=10,
         printlog=True,
@@ -284,4 +328,4 @@ if __name__ == '__main__':
     # Run over everything
     cerebro.run(maxcpus=4)
 
-    # cerebro.plot()
+    cerebro.plot()
